391811043e48c444d8c3dc91bc0636f2a1a98c0f
hackedlings <pks9862728888@gmail.com>
2022-03-20 13:18:04 +0530

    Initial commit

---
+++ src/main/java/com/fluorescence/IOUtils/IoUtilsApplication.java
@@ -0,0 +1,13 @@
+package com.fluorescence.IOUtils;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IoUtilsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IoUtilsApplication.class, args);
+	}
+
+}
